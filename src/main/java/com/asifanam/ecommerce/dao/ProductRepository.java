68f1a4f3b690b4ace810b3bb7521ec9b1bf5ed98
ASIF ANAM SHAON <anam1828@cseku.ac.bd>
2023-05-22 02:19:10 +0600

    udemy section 13

--- src/main/java/com/asifanam/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/asifanam/ecommerce/dao/ProductRepository.java
@@ -1,11 +1,13 @@
 package com.asifanam.ecommerce.dao;
 
 import com.asifanam.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-import java.awt.print.Pageable;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
